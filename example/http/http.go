cb771fec52d0c5a37bf2181d935c31f165b29518
Shailendra Sharma <shailendra.sharma@gmail.com>
2024-10-31 14:46:07 +0530

    wip wasm changes

--- example/http/http.go
+++ example/http/http.go
@@ -17,103 +17,128 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"github.com/deepflowio/deepflow-wasm-go-sdk/sdk"
-	"github.com/google/uuid"
-	"log"
-	"net/http"
-	"strings"
+	"slices"
 )
 
-type httpHook struct {
+type parser struct {
 }
 
-func (p httpHook) HookIn() []sdk.HookBitmap {
+func (p parser) HookIn() []sdk.HookBitmap {
 	return []sdk.HookBitmap{
-		sdk.HOOK_POINT_HTTP_REQ,
-		sdk.HOOK_POINT_HTTP_RESP,
+		//sdk.HOOK_POINT_HTTP_REQ,
+		sdk.HOOK_POINT_PAYLOAD_PARSE,
+		//sdk.HOOK_POINT_HTTP_RESP,
 	}
 }
 
-/*
-assume the http request as follow:
-
-	GET /user_info?username=test&type=1 HTTP/1.1
-	Custom-Trace-Info: trace_id: xxx, span_id: sss
-*/
-func (p httpHook) OnHttpReq(ctx *sdk.HttpReqCtx) sdk.Action {
-	baseCtx := &ctx.BaseCtx
-	/*if baseCtx.DstPort != 8080 || !strings.HasPrefix(ctx.Path, "/user_info?") {
-		return sdk.ActionNext()
-	}*/
-
-	payload, err := baseCtx.GetPayload()
-	if err != nil {
-		return sdk.ActionAbortWithErr(err)
-	}
-
-	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(payload)))
-	if err != nil {
-		return sdk.ActionAbortWithErr(err)
-	}
-
-	//	query := req.URL.Query()
-
-	/*	attr := []sdk.KeyVal{
-		{
-			Key: "username",
-			Val: query.Get("username"),
-		},
-
-		{
-			Key: "type",
-			Val: query.Get("type"),
-		},
-	}*/
-
-	attr := []sdk.KeyVal{}
-
-	var (
-		traceID      string
-		spanID       string
-		parentSpanID string
-		trace        *sdk.Trace
-	)
-
-	traceInfo, ok := req.Header["Custom-Trace-Info"]
-	if ok && len(traceInfo) != 0 {
-		s := strings.Split(traceInfo[0], ":")
-		if len(s) == 3 {
-			traceID = strings.TrimSpace(s[0])
-			spanID = strings.TrimSpace(s[1])
-			parentSpanID = strings.TrimSpace(s[2])
-
-		}
-
-	}
-
-	if traceID != "" && spanID != "" {
-		trace = &sdk.Trace{
-			TraceID:      traceID,
-			SpanID:       spanID,
-			ParentSpanID: parentSpanID,
-		}
-	} else {
-		traceId, err := uuid.NewUUID()
-		if err != nil {
-			log.Fatalf("failed to generate UUID: %v", err)
-		}
-		s := traceId.String()
-		trace = &sdk.Trace{
-			TraceID: s,
-			SpanID:  s,
-		}
-
-	}
-
-	return sdk.HttpReqActionAbortWithResult(nil, trace, attr)
+func (p parser) OnHttpReq(ctx *sdk.HttpReqCtx) sdk.Action {
+	return sdk.ActionNext()
 }
+
+//func (p parser) OnHttpReq(ctx *sdk.HttpReqCtx) sdk.Action {
+//	baseCtx := &ctx.BaseCtx
+//
+//	payload, err := baseCtx.GetPayload()
+//	if err != nil {
+//		return sdk.ActionAbortWithErr(err)
+//	}
+//
+//	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(payload)))
+//	if err != nil {
+//		return sdk.ActionAbortWithErr(err)
+//	}
+//
+//	excludeNames := []string{
+//		"Accept",
+//		"Connection",
+//		"User-Agent",
+//		"Accept-Encoding",
+//		"Metadata",
+//		"X-Ms-Agent-Name",
+//		"X-Ms-Version",
+//		"X-Ms-Containerid",
+//		"X-Ms-Host-Config-Name",
+//		"X-Prometheus-Scrape-Timeout-Seconds",
+//		"X-Prometheus-Scrape-Timeout-Seconds",
+//		"X-Forwarded-For",
+//		"X-Forwarded-Uri",
+//		"Content-Length",
+//		"Content-Type",
+//		"Referer",
+//		"Accept-Language",
+//		"Upgrade-Insecure-Requests",
+//		"Cookie",
+//		"X-User-Id",
+//		"X-User-Type",
+//		"Origin",
+//		"Cache-Control",
+//		"Sec-Websocket-Extensions",
+//		"Pragma",
+//		"Sec-Websocket-Version",
+//		"Sec-Websocket-Key",
+//	}
+//	// Loop over header names
+//	for name, values := range req.Header {
+//		if slices.Contains(excludeNames, name) {
+//			continue
+//		}
+//		// Loop over all values for the name.
+//		for _, value := range values {
+//			sdk.Warn("got header", name, value, req.URL.Path)
+//		}
+//	}
+//
+//	//var (
+//	//	traceID      string
+//	//	spanID       string
+//	//	parentSpanID string
+//	//	trace        *sdk.Trace
+//	//)
+//	//
+//	//key := "Custom-Trace-Info"
+//	//
+//	//traceInfo, ok := req.Header[key]
+//	//if ok && len(traceInfo) != 0 {
+//	//	sdk.Warn("got trace info from header", traceInfo)
+//	//	s := strings.Split(traceInfo[0], ":")
+//	//	if len(s) == 3 {
+//	//		traceID = strings.TrimSpace(s[0])
+//	//		spanID = strings.TrimSpace(s[1])
+//	//		parentSpanID = strings.TrimSpace(s[2])
+//	//	}
+//	//}
+//	//
+//	//if traceID != "" && spanID != "" {
+//	//	trace = &sdk.Trace{
+//	//		TraceID:      traceID,
+//	//		SpanID:       spanID,
+//	//		ParentSpanID: parentSpanID,
+//	//	}
+//	//} else {
+//	//	s := strconv.FormatUint(baseCtx.FlowID, 10)
+//	//	trace = &sdk.Trace{
+//	//		TraceID: s,
+//	//		SpanID:  s,
+//	//	}
+//	//}
+//	//
+//	//val := trace.TraceID + ":" + trace.SpanID + ":" + trace.ParentSpanID
+//	//
+//	//req.Header.Set(key, val)
+//	//attr := []sdk.KeyVal{
+//	//	{
+//	//		Key: key,
+//	//		Val: val,
+//	//	},
+//	//}
+//
+//	//return sdk.HttpReqActionAbortWithResult(nil, trace, attr)
+//	return sdk.ActionAbort()
+//}
 
 /*
 assume resp as follow:
@@ -123,12 +148,11 @@ assume resp as follow:
 	{"code": 0, "data": {"user_id": 12345, "register_time": 1682050409}}
 */
 
-func (p httpHook) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
+func (p parser) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
 	return sdk.ActionNext()
-
 }
 
-/*func (p httpHook) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
+/*func (p parser) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
 	baseCtx := &ctx.BaseCtx
 	if baseCtx.SrcPort != 8080 {
 		return sdk.ActionNext()
@@ -170,16 +194,113 @@ func (p httpHook) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
 
 }*/
 
-func (p httpHook) OnCheckPayload(baseCtx *sdk.ParseCtx) (uint8, string) {
+func (p parser) OnCheckPayload(baseCtx *sdk.ParseCtx) (uint8, string) {
 	return 0, ""
 }
 
-func (p httpHook) OnParsePayload(baseCtx *sdk.ParseCtx) sdk.Action {
-	return sdk.ActionNext()
+//func (p parser) OnParsePayload(baseCtx *sdk.ParseCtx) sdk.Action {
+//	return sdk.ActionNext()
+//}
+
+const (
+	HTTP_PROTOCOL          = 0
+	GO_HTTP2_EBPF_PROTOCOL = 1
+)
+
+func (p parser) OnParsePayload(ctx *sdk.ParseCtx) sdk.Action {
+	payload, err := ctx.GetPayload()
+	if err != nil {
+		return sdk.ActionAbortWithErr(err)
+	}
+
+	excludeNames := []string{
+		"Accept",
+		"Connection",
+		"User-Agent",
+		"Accept-Encoding",
+		"Metadata",
+		"X-Ms-Agent-Name",
+		"X-Ms-Version",
+		"X-Ms-Containerid",
+		"X-Ms-Host-Config-Name",
+		"X-Prometheus-Scrape-Timeout-Seconds",
+		"X-Prometheus-Scrape-Timeout-Seconds",
+		"X-Forwarded-For",
+		"X-Forwarded-Uri",
+		"Content-Length",
+		"Content-Type",
+		"Referer",
+		"Accept-Language",
+		"Upgrade-Insecure-Requests",
+		"Cookie",
+		"X-User-Id",
+		"X-User-Type",
+		"Origin",
+		"Cache-Control",
+		"Sec-Websocket-Extensions",
+		"Pragma",
+		"Sec-Websocket-Version",
+		"Sec-Websocket-Key",
+	}
+
+	switch ctx.EbpfType {
+	case sdk.EbpfTypeGoHttp2Uprobe:
+		streamID, key, val, err := parseHeader(payload)
+		if err != nil {
+			return sdk.ActionAbortWithErr(err)
+		}
+
+		if slices.Contains(excludeNames, key) {
+			return sdk.ActionAbort()
+		}
+
+		sdk.Warn("EbpfTypeGoHttp2Uprobe -- StreamID/Key/Val", streamID, key, val)
+		return sdk.ActionAbort()
+	default:
+		//req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(payload)))
+		//if err != nil {
+		//	return sdk.ActionAbortWithErr(err)
+		//}
+		//
+		//// Loop over header names
+		//for name, values := range req.Header {
+		//	if slices.Contains(excludeNames, name) {
+		//		continue
+		//	}
+		//	// Loop over all values for the name.
+		//	for _, value := range values {
+		//		sdk.Warn("got header", name, value, req.URL.Path)
+		//	}
+		//}
+
+		return sdk.ActionAbort()
+	}
+}
+
+/*
+fd(4 bytes)
+stream id (4 bytes)
+header key len (4 bytes)
+header value len (4 bytes)
+header key value (xxx bytes)
+header value value (xxx bytes)
+*/
+func parseHeader(payload []byte) (uint32, string, string, error) {
+	if len(payload) < 16 {
+		return 0, "", "", errors.New("header payload too short")
+	}
+
+	streamID := binary.LittleEndian.Uint32(payload[4:8])
+	keyLen := int(binary.LittleEndian.Uint32(payload[8:12]))
+	valLen := int(binary.LittleEndian.Uint32(payload[12:16]))
+	if keyLen < 0 || keyLen < 0 || keyLen+valLen+16 > len(payload) {
+		return 0, "", "", fmt.Errorf("header kv length too short, key len: %d, val len: %d, payload len: %d", keyLen, valLen, len(payload))
+	}
+
+	return streamID, string(payload[16 : 16+keyLen]), string(payload[16+keyLen : 16+keyLen+valLen]), nil
 }
 
 func main() {
 	sdk.Warn("wasm register http hook")
-	sdk.SetParser(httpHook{})
-
+	sdk.SetParser(parser{})
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/valyala/fastjson v1.6.4
 	golang.org/x/net v0.9.0
 	google.golang.org/protobuf v1.30.0
+        
 )
 
 require github.com/google/uuid v1.4.0 // indirect
